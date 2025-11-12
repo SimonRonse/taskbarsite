a288f2ab76f8de4ea495505255d3dc9bc0f48058
SimonRonse <ronse.simon@gmail.com>
2025-11-13 00:02:13 +0100

    add Jenkins pipeline for frontend deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../dist",      // adjust if you want output outside frontend/
+    outDir: "dist",      // adjust if you want output outside frontend/
   },
 })
